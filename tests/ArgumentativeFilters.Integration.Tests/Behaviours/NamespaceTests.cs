fa92b6bea5475911fc0a64ebfaf59691f835c970
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2023-04-02 19:33:29 +0100

    Start working on integration tests
    Need to have full hierarchy building built for it to work first

---
+++ tests/ArgumentativeFilters.Integration.Tests/Behaviours/NamespaceTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ArgumentativeFilters.Integration.Tests.Behaviours;
+
+public partial class NamespaceTests : ArgumentativeFilterTests
+{
+    public NamespaceTests()
+    {
+        Initialize((string test) => test, new List<object?> { "testValue" });
+    }
+
+    // internal static partial class TestFilterParent
+    // {
+    //     [ArgumentativeFilter]
+    //     internal static async ValueTask<object?> TestFilter(EndpointFilterInvocationContext context, EndpointFilterDelegate next, string test)
+    //     {
+    //         return await next(context);
+    //     }
+    // }
+}
