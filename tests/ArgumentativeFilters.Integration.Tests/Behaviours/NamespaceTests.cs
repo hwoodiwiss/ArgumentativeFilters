ba9622daf26b8ddc601730f4c87319f024fab44a
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2023-04-07 19:28:53 +0100

    Fix nullable value types

--- tests/ArgumentativeFilters.Integration.Tests/Behaviours/NamespaceTests.cs
+++ tests/ArgumentativeFilters.Integration.Tests/Behaviours/NamespaceTests.cs
@@ -6,15 +6,15 @@ public partial class NamespaceTests : ArgumentativeFilterTests
 {
     public NamespaceTests()
     {
-        Initialize((string test) => test, new List<object?> { "testValue" });
+        Initialize((int? test) => test, new List<object?> { "testValue" });
     }
 
-    // internal static partial class TestFilterParent
-    // {
-    //     [ArgumentativeFilter]
-    //     internal static async ValueTask<object?> TestFilter(EndpointFilterInvocationContext context, EndpointFilterDelegate next, string test)
-    //     {
-    //         return await next(context);
-    //     }
-    // }
+    internal static partial class TestFilterParent
+    {
+        [ArgumentativeFilter]
+        private static async ValueTask<object?> TestFilter(EndpointFilterInvocationContext context, EndpointFilterDelegate next, string test)
+        {
+            return await next(context);
+        }
+    }
 }
