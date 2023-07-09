abd22d9d5db8f74b9b4994f1b2264c2ec9db6fc2
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2023-07-09 16:06:10 +0100

    Change docs for GetArgumentIndex

--- src/ArgumentativeFilters.Generator/CodeGeneration/ConstantTypeCode.cs
+++ src/ArgumentativeFilters.Generator/CodeGeneration/ConstantTypeCode.cs
@@ -13,9 +13,9 @@ namespace ArgumentativeFilters
         /// <summary>
         /// A static helper method for finding the index of a named parameter on a MinimalAPI endpoint, given an <see cref="global::Microsoft.AspNetCore.Http.EndpointFilterFactoryContext" />.
         /// </summary>
-        /// <param name="context">The <see cref="global::Microsoft.AspNetCore.Http.EndpointFilterFactoryContext" /> within which to search for the named parameter.</param>
-        /// <param name="argumentName">The argument name to search for within the <see cref="global::Microsoft.AspNetCore.Http.EndpointFilterFactoryContext" />.</param>
-        /// <param name="comparisonMode">The string comparison mode to use when searching for parameter indices.</param>
+        /// <param name="context">The <see cref="global::Microsoft.AspNetCore.Http.EndpointFilterFactoryContext" /> to search for the provided argument names index in.</param>
+        /// <param name="argumentName">The argument name to search for in the <see cref="global::Microsoft.AspNetCore.Http.EndpointFilterFactoryContext" />'s <c>MethodInfo</c> property.</param>
+        /// <param name="comparisonMode">The string comparison mode to use when searching for argument indices.</param>
         /// <returns>
         /// The endpoint delegate parameter index for the given argument name as a <see cref="int?" />.
         /// Returns <see langword="null"/> if the argument name is not found.
