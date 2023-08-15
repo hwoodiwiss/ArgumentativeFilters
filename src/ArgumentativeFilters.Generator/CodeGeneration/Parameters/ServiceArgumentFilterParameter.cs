0e0e5269d92f2e6ab0595ce352c98b30af8d5c64
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2023-08-15 19:38:32 +0100

    Add support for keyed services

--- src/ArgumentativeFilters.Generator/CodeGeneration/Parameters/ServiceArgumentFilterParameter.cs
+++ src/ArgumentativeFilters.Generator/CodeGeneration/Parameters/ServiceArgumentFilterParameter.cs
@@ -7,22 +7,24 @@ public class ServiceArgumentFilterParameter : ArgumentativeFilterParameterProvid
     private readonly string _argumentName;
     private readonly string _argumentType;
     private readonly bool _required;
+    private readonly string? _key;
     private const string NameSuffix = "Service";
-    private const string GetRequiredServiceMethodName = "GetRequiredService";
-    private const string GetServiceMethodName = "GetService";
 
-    public ServiceArgumentFilterParameter(string argumentName, string argumentType, bool required)
+    public ServiceArgumentFilterParameter(string argumentName, string argumentType, bool required, string? key)
     {
         _argumentName = argumentName;
         _argumentType = argumentType;
         _required = required;
+        _key = key;
     }
 
-    public string FilterCode => $"{_argumentType}{(_required ? string.Empty : "?")} {_argumentName}{NameSuffix} = {VariableNames.InvocationFilterContext}.HttpContext.RequestServices.{GetServiceProviderMethodName()}<{_argumentType}>();";
+    public string FilterCode => $"{_argumentType}{(_required ? string.Empty : "?")} {_argumentName}{NameSuffix} = {VariableNames.InvocationFilterContext}.HttpContext.RequestServices.{GetServiceProviderMethodName()}<{_argumentType}>({_key});";
 
     public override string ParameterCode => $"{_argumentName}{NameSuffix}";
 
-    private string GetServiceProviderMethodName() => _required
-        ? GetRequiredServiceMethodName
-        : GetServiceMethodName;
+    private string GetServiceProviderMethodName() 
+        => $"Get{ValueOrEmpty(_required, "Required")}{ValueOrEmpty(_key is not null, "Keyed")}Service";
+    
+    private static string ValueOrEmpty(bool condition, string value) 
+        => condition ? value : string.Empty;
 }
